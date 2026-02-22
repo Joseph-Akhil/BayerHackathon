00063abd782599f4a74b685031b4de4f909c8ff1
ajoassociates <info.ajoassociates@gmail.com>
2026-02-22 13:58:01 +0530

    Updating terraform

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -118,4 +118,4 @@ resource "aws_route_table_association" "private_a_assoc" {
 resource "aws_route_table_association" "private_b_assoc" {
   subnet_id      = aws_subnet.private_b.id
   route_table_id = aws_route_table.private_rt.id
-}
+} 
